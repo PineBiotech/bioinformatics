697ec76e9242985eb4e23a182d01c64c0864de94
PineBio <elia@pine-biotech.com>
2020-09-07 06:29:17 -0500

    Add files via upload

--- metagenomics_part1.R
+++ metagenomics_part1.R
@@ -1,19 +1,30 @@
 #Test your R code here
 
 library(openxlsx)
+library(dplyr)
 
 #load data
-bacteria_order <- read.xlsx("metagenomics1Annotation.xlsx", sheet = 3)
+annotatedtb <- read.xlsx("metagenomics1Annotation.xlsx", sheet = 4)
+df <- data.frame(annotatedtb)
 
-#create a table of results
-numtable <- table(bacteria_order$ERR1250035)
+#create a table of summary values by phylum
+numtable1 <- rowsum(df$ERR1250035, df$phylum, reorder = FALSE)
+numtable1 <- na.omit(numtable1)
+totalsum <- sum(numtable1)
+numtable2 <- lapply(numtable1[,1], function(x) { x/totalsum })
+numtable2 <- lapply(numtable2, function(x) { round(x*100, digits=2)})
 
-prop.table(numtable)
-labels <- unique(as.vector(bacteria_order$byphylum))
 
-print(labels)
+#add labels and count length
+labels <- row.names(numtable1)
 colnum <- length(labels)
+colorfill <- factor(c(labels))
+numtable2 <- lapply(numtable2, function(x) { paste(colorfill[x], x, " %")})
 
-par(mar=c(2,2,2,2))
-pie(numtable, col=heat.colors(colnum), main="Summary Pie Chart", c(labels))
-
+#make a pie chart
+par(mar=c(4,4,4,4))
+par(xpd=TRUE)
+pie(table(annotatedtb[,1]), col = rainbow(colnum), radius = 01, main="ERR1250035 by Phylum", c(numtable2), cex = 0.6)
+legend(-2,1,labels,c(rainbow(colnum)), cex = 0.5)
+par(mar=c(6,4,4,4))
+barplot(table(annotatedtb[,1]), col = rainbow(colnum), main="ERR1250035 by Phylum", xlab = "Bacteria Type", las = 2, cex.names = 0.5)
